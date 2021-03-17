5bada6cc8d7e364f469ed0f3b6fc6e5611239a16
Gabriela Lopez Diaz <lopezgaby811@gmail.com>
2021-03-16 18:23:44 -0700

    Add project files.

---
+++ HW5/main.cpp
@@ -0,0 +1,55 @@
+// Participants: Gabriela Lopez, Andrew Thompson, Long Duong, Christopher Gomez, Gia Minh Hoang, Duyen Tran and Lincoln Nguyen
+// Date: 03-16-21
+// Description: Chapter 5 Solution
+
+#include "input.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Precondition: N/A
+//Postcondition: Prints menu and returns user int option selected
+int menuOption();
+
+int main()
+{
+	do
+	{
+		switch (menuOption())
+		{
+		case 0:  exit(0);
+		case 1:  system("cls"); /*vector();*/ break;
+		case 2:  system("cls"); /*list();*/ break;
+		case 3:  system("cls"); /*vector/list();*/ break;
+		default: cout << "\t\t    ERROR-Invalid Option. Please re-enter."; break;
+		}
+		cout << '\n';
+		system("pause");
+	} while (true);
+	return 0;
+}
+
+int menuOption()
+{
+	system("cls");
+	cout << '\n';
+	cout << '\t' << char(201) << string(42, char(205)) << char(187) << '\n';
+	cout << '\t' << char(186) << "    \t    CMPR131: Chapter 5             " << char(186) << '\n';
+	cout << '\t' << char(186) << "      \t\t  Made by:                 " << char(186) << '\n';
+	cout << '\t' << char(186) << " - Gabriela Lopez\t- Andrew Thompson  " << char(186) << '\n';
+	cout << '\t' << char(186) << " - Lincoln Nguyen\t- Long Duong       " << char(186) << '\n';
+	cout << '\t' << char(186) << " - Christopher Gomez\t- Gia Minh Hoang   " << char(186) << '\n';
+	cout << '\t' << char(186) << " - Duyen Tran                             " << char(186) << '\n';
+	cout << "\t" << char(204) << string(42, char(205)) << char(185) << "\n";
+	cout << "\t" << char(186) << "   1: Vector Container                    " << char(186) << "\n";
+	cout << "\t" << char(186) << "   2: List Container                      " << char(186) << "\n";
+	cout << "\t" << char(186) << "   3: Aplication using Vector or List Container   " << char(186) << "\n";
+	cout << "\t" << char(199) << string(42, char(196)) << char(182) << "\n";
+	cout << "\t" << char(186) << "   0: Exit                                " << char(186) << "\n";
+	cout << "\t" << char(200) << string(42, char(205)) << char(188) << "\n";
+
+	int option = inputInteger("\t    Option: ", 0, 3);
+	cin.clear();
+	return option;
+}
